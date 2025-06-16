e33f182c5d0e4d801ebf02c870dc133029dc89eb
Tushar-Chavan14 <tusharchavan166@gmail.com>
2025-06-17 03:00:13 +0530

    build fix

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,15 +1,9 @@
-import { TextEditorWidowModalExp } from "@src/components/modals/windows/textEditorWindow";
-
 const Placeholder = async ({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) => {
   const routeParams = await params;
-
-  if (routeParams?.slug === "window") {
-    return <TextEditorWidowModalExp />;
-  }
 
   return (
     <div>
